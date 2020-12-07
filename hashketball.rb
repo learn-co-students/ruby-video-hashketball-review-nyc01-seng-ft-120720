59bc5e7a9d408cef751653a2974e530c3ae2e3d3
Daniel Sasse <dsasse07@gmail.com>
2020-12-07 00:28:20 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,3 +128,162 @@ def game_hash
 end
 
 # Write code here
+
+def num_points_scored(player_name)
+  search_player_data(player_name,:points)
+end
+
+def shoe_size (player_name)
+  search_player_data(player_name,:shoe)
+end
+
+def team_colors (player_name)
+  search_team_data(player_name, :colors)
+end
+  
+def team_names
+  team_names_list = []
+  game_hash.each_key do |location|
+    team_names_list << game_hash[location][:team_name]
+  end
+  team_names_list
+end
+
+def player_numbers (team_name)
+  player_numbers = []
+  roster = []
+  
+  player_data_aoh = search_team_data(team_name, :players)
+  player_data_aoh.each do |player|
+    player_numbers << player[:number]
+  end
+  player_numbers
+end
+
+def player_stats (player_name)
+  team_names_array = team_names
+  player_stats_hash = {}
+  
+  team_names_array.each do |team|
+    roster = search_team_data(team,:players)
+    roster.each do |player_entry|
+      if player_entry[:player_name] == player_name
+        player_stats_hash = player_entry
+        return player_stats_hash
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  stat_by_player = collect_stat_by_name(:shoe)
+  player_with_max_stat = find_max_stat_by_player(stat_by_player)
+  num_rebounds = search_player_data(player_with_max_stat,:rebounds)
+  num_rebounds
+end
+
+#
+#Below are helper methods
+#
+
+def collect_stat_by_name (stat) #returns hash of "name" => stat value
+  team_names_array = team_names
+  player_stats_array = []
+  roster = []
+  player_stat = {}
+  
+  team_names_array.each do |team|
+    roster << search_team_data(team, :players)
+    roster.flatten!
+    roster.each do |player_entry|
+      player_stats_array << player_entry.slice(:player_name, stat)
+    end
+  end
+    player_stats_array.each do |player_stat_hash|
+      player_stat[player_stat_hash[:player_name]] = player_stat_hash[stat]
+    end
+  player_stat
+end
+
+def find_max_stat_by_player(stat_by_player)
+ sorted_stat_array = stat_by_player.sort_by { |name, stat| stat}
+ player_with_max_stat = sorted_stat_array.last[0]
+end
+  
+
+def search_team_data (team_name, desired_team_data)
+  game_hash.each do |location, attribute|
+    if game_hash[location][:team_name] == team_name
+      returned_team_data = game_hash[location][desired_team_data]
+      return returned_team_data
+    end
+  end
+end
+
+def search_player_data (player_name, desired_player_data)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if data.class == Array
+        index=0
+        while data[index].class == Hash
+          i = 0
+          while i < data.length do 
+            if data[i][:player_name]==player_name
+              returned_player_data = data[i][desired_player_data]
+              return returned_player_data
+              # binding.pry
+            end
+            i += 1 
+          end
+          index += 1
+        end 
+      end
+    end
+  end
+end
+  
+#
+# Below are bonus methods
+#
+
+def most_points_scored
+  stat_by_player = collect_stat_by_name(:points)
+  player_with_max_stat = find_max_stat_by_player(stat_by_player)
+  player_with_max_stat
+end
+
+def winning_team
+  team_array = team_names
+  team_points = [0,0]
+  
+  i = 0
+  while i < team_array.length do
+    player_data = search_team_data(team_array[i],:players)
+    
+    player_data.each do |single_player|
+      points = single_player[:points]
+      team_points[i] += points
+    end
+    i += 1
+  end
+  team_array[team_points.index((team_points.max))] + " is the winner!"
+end
+
+def player_with_longest_name
+  stat_by_player = collect_stat_by_name(:player_name)
+  name_length_values = stat_by_player.each_with_object ({}) { |(k,v),hash| hash[k] = v.length}
+  longest_name = find_max_stat_by_player(name_length_values)
+  longest_name
+end
+
+def long_name_steals_a_ton?
+  stat_by_player = collect_stat_by_name(:steals)
+  player_with_max_stat = find_max_stat_by_player(stat_by_player)
+  long_name = player_with_longest_name
+  if player_with_max_stat == player_with_longest_name
+    result = true 
+  else
+    result = false
+  end
+  result
+end 
